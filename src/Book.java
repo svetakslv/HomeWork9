d9fc46009a965f92ca03053592f77cecd7121737
Светка <sveta.chakrygina@mail.ru>
2022-09-03 22:42:10 +0300

    Main, Book

--- src/Book.java
+++ src/Book.java
@@ -2,12 +2,9 @@ public class Book {
     String name;
     int year;
 
-
-    public static Book Book(String name, int year) {
-        Book book = new Book();
-        book.name = name;
-        book.year = year;
-        return book;
+    public Book(String name, int year) {
+        this.name = name;
+        this.year = year;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        Book horror = Book.Book("Pet Sematary",1983);
+        Book horror = new Book("Pet Sematary",1983);
         System.out.println("horror.name = " + horror.name);
         System.out.println("horror.year = " + horror.year);
+
+
+        Book novel = new Book("The Alchemist", 1988);
+        System.out.println("novel.name = " + novel.name);
+        System.out.println("novel.year = " + novel.year);
 
 
     }
